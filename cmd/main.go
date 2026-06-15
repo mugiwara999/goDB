e3560f2f60b11a7c4c6ecef08298bc1767099d03
mugiwara999 <125131353+mugiwara999@users.noreply.github.com>
2026-06-15 09:26:02 +0530

    add update value function

--- cmd/main.go
+++ cmd/main.go
@@ -84,7 +84,7 @@ func main() {
 
 	defer tableInstance.Close()
 
-	fmt.Println("Enter 'insert' or 'delete' or 'select' or 'exit' to perform operations on the table")
+	fmt.Println("Enter 'insert' or 'delete' or 'select' or 'update' or 'exit' to perform operations on the table")
 	for scanner.Scan() {
 
 		if scanner.Err() != nil {
@@ -97,7 +97,7 @@ func main() {
 			return
 		}
 
-		if command != "insert" && command != "delete" && command != "select" {
+		if command != "insert" && command != "delete" && command != "select" && command != "update" {
 			fmt.Println("Invalid command")
 			return
 		}
@@ -213,8 +213,73 @@ func main() {
 
 				colEquals = append(colEquals, x)
 			}
+		case "update":
+
+			fmt.Println("Enter column name and value to filter by in the format 'column=value (type 'exit' to finish)")
+			var colEquals []table.ColEq
+			var toUpdate []table.UpdateValue
+			for scanner.Scan() {
+				text := scanner.Text()
+
+				if text == "exit" {
+					slices.SortFunc(colEquals, func(a, b table.ColEq) int {
+						return a.ColIdx - b.ColIdx
+
+					})
+					fmt.Println("Enter column name and value to update in the format 'column=value'")
+					for scanner.Scan() {
+						text := scanner.Text()
+						if text == "exit" {
+							err := tableInstance.Update(colEquals, toUpdate)
+							if err != nil {
+								fmt.Println(err)
+							}
+							fmt.Println("Update successful")
+							break
+						}
+						parts := strings.SplitN(text, "=", 2)
+						if len(parts) != 2 {
+							fmt.Println(table.ErrorInvalidInput, "Expected format heeee 'column=value'")
+							continue
+						}
+
+						colName := parts[0]
+						colIdx := slices.Index(tableInstance.GetColumns(), colName)
+						if colIdx == -1 {
+							fmt.Println(table.ErrorInvalidInput, "Column name not found")
+							continue
+						}
+						newVal := parts[1]
+
+						toUpdate = append(toUpdate, table.UpdateValue{
+							ColIdx: colIdx,
+							Value:  newVal,
+						})
+					}
+					break
+				}
+
+				parts := strings.SplitN(text, "=", 2)
+				if len(parts) != 2 {
+					fmt.Println(table.ErrorInvalidInput, "Expected format 'column=value'")
+					continue
+				}
+				colName := parts[0]
+				colIdx := slices.Index(tableInstance.GetColumns(), colName)
+				if colIdx == -1 {
+					fmt.Println(table.ErrorInvalidInput, "Column name not found")
+					continue
+				}
+				x := table.ColEq{
+					ColIdx: colIdx,
+					Value:  parts[1],
+				}
+
+				colEquals = append(colEquals, x)
+			}
+
 		}
-		fmt.Println("Enter 'insert' or 'delete' or 'select' or 'exit' to perform operations on the table")
+		fmt.Println("Enter 'insert' or 'delete' or 'select' or 'update' or 'exit' to perform operations on the table")
 	}
 
 }
