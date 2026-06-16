b85fd9d3b58fe85a96b02e1b79067fd058cf13c2
mugiwara999 <125131353+mugiwara999@users.noreply.github.com>
2026-06-16 15:39:59 +0530

    add insert, create and update SQL parsing, complete CRUD via SQL interface

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -36,6 +36,7 @@ func Parse(input string) (*Query, error) {
 	case "exit":
 		return nil, nil
 
+		// select col1,col2,col3 from table where name1=value1 and name2=value2
 	case "select":
 		pos := 0
 		curr := 0
@@ -86,7 +87,159 @@ func Parse(input string) (*Query, error) {
 
 		return query, nil
 
-	}
+		// delete from table where name1=value1 and name2=value2
+	case "delete":
+		pos := 0
+		curr := 0
+		query.Type = "delete"
+		curr++
+		pos++
+		if curr >= len(tokens) || tokens[curr] != "from" {
+			return nil, table.ErrorInvalidInput
+		}
 
-	return nil, nil
+		curr++
+		pos++
+		if curr >= len(tokens) {
+			return nil, table.ErrorInvalidInput
+		}
+		query.Table = tokens[curr]
+		curr++
+		pos++
+
+		query.Filters = []Pair{}
+		if len(tokens) > curr && tokens[curr] == "where" {
+			// assuming only AND conditions
+
+			for i := curr + 1; i < len(tokens); i += 2 {
+
+				colName, colValue, ok := strings.Cut(tokens[i], "=")
+
+				if !ok {
+					return nil, table.ErrorInvalidInput
+				}
+				curr = i
+				query.Filters = append(query.Filters, Pair{colName, colValue})
+
+			}
+		}
+		return query, nil
+
+	// insert into table values val1,val2,val3
+	case "insert":
+		pos := 0
+		curr := 0
+		query.Type = "insert"
+		curr++
+		pos++
+		if curr >= len(tokens) || tokens[curr] != "into" {
+			return nil, table.ErrorInvalidInput
+		}
+		curr++
+		pos++
+		if curr >= len(tokens) {
+			return nil, table.ErrorInvalidInput
+		}
+		query.Table = tokens[curr]
+		curr++
+		pos++
+
+		if curr >= len(tokens) || tokens[curr] != "values" {
+			return nil, table.ErrorInvalidInput
+		}
+
+		curr++
+		pos++
+
+		if curr >= len(tokens) {
+			return nil, table.ErrorInvalidInput
+		}
+		query.Values = strings.Split(strings.Join(tokens[pos:], ""), ",")
+		return query, nil
+
+	// update table set col1=value1, col2=value2 where name1=value1 and name2=value2
+	case "update":
+		pos := 0
+		curr := 0
+		query.Type = "update"
+		curr++
+		pos++
+		if curr >= len(tokens) {
+			return nil, table.ErrorInvalidInput
+		}
+		query.Table = tokens[curr]
+		curr++
+		pos++
+
+		if curr >= len(tokens) || tokens[curr] != "set" {
+			return nil, table.ErrorInvalidInput
+		}
+		curr++
+		pos++
+
+		query.Updates = []Pair{}
+		for curr < len(tokens) && tokens[curr] != "where" {
+			colName, colValue, ok := strings.Cut(tokens[curr], "=")
+
+			if !ok {
+				return nil, table.ErrorInvalidInput
+			}
+
+			if strings.HasSuffix(colName, ",") {
+				colValue = colValue[:len(colValue)-1]
+			}
+
+			query.Updates = append(query.Updates, Pair{colName, colValue})
+			curr++
+		}
+
+		query.Filters = []Pair{}
+		if len(tokens) > curr && tokens[curr] == "where" {
+			// assuming only AND conditions
+
+			for i := curr + 1; i < len(tokens); i += 2 {
+
+				colName, colValue, ok := strings.Cut(tokens[i], "=")
+
+				if !ok {
+					return nil, table.ErrorInvalidInput
+				}
+				curr = i
+				query.Filters = append(query.Filters, Pair{colName, colValue})
+			}
+
+		}
+
+	case "create":
+		pos := 0
+		curr := 0
+		query.Type = "create"
+		curr++
+		pos++
+		if curr >= len(tokens) || tokens[curr] != "table" {
+			return nil, table.ErrorInvalidInput
+		}
+		curr++
+		pos++
+		if curr >= len(tokens) {
+			return nil, table.ErrorInvalidInput
+		}
+		query.Table = tokens[curr]
+		curr++
+		pos++
+
+		if curr >= len(tokens) || tokens[curr] != "columns" {
+			return nil, table.ErrorInvalidInput
+		}
+		curr++
+		pos++
+
+		if curr >= len(tokens) {
+			return nil, table.ErrorInvalidInput
+		}
+		query.Columns = strings.Split(strings.Join(tokens[pos:], ""), ",")
+
+	}
+	return query, nil
+
 }
