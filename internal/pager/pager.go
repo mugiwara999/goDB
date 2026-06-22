17e745bdc2e858ba4c1c5a1214938d14fab7763a
mugiwara999 <125131353+mugiwara999@users.noreply.github.com>
2026-06-22 11:44:09 +0530

    change structure of meta-data page

--- internal/pager/pager.go
+++ internal/pager/pager.go
@@ -28,12 +28,7 @@ func NewPager(path string) (*Pager, error) {
 
 func (p *Pager) GetPage(pageID int) (*Page, error) {
 	offset := pageID * PAGE_SIZE
-	_, err := p.file.Seek(0, 0)
-
-	if err != nil {
-		return nil, ErrorReadingPage
-	}
-	_, err = p.file.Seek(int64(offset), 0)
+	_, err := p.file.Seek(int64(offset), 0)
 
 	if err != nil {
 		return nil, ErrorReadingPage
@@ -78,35 +73,35 @@ func (p *Pager) Flush(page *Page) error {
 // meta data
 // Bytes 0-3   : GODB
 // Bytes 4-5   : version
-// Bytes 6-9  : numPages
+// Bytes 6-9  : numCols
 // Bytes 10+ : column names (null-terminated strings)
 
 func (p *Pager) GetColumns() []string {
+	if p.numPages == 0 {
+		return nil
+	}
 	page, err := p.GetPage(0)
 
 	if err != nil || page == nil {
 		return nil
 	}
 
-	info, err := p.file.Stat()
-
-	if err != nil {
-		return nil
-	}
-
-	numCols := int(info.Size() / PAGE_SIZE)
+	numCols := int(binary.LittleEndian.Uint32(page.Data[6:10]))
 
 	cols := make([]string, numCols)
 
 	accumulator := make([]byte, 0)
 
+	offset := 10
+
 	colIdx := 0
 
-	for i := 12; i < PAGE_SIZE && colIdx < numCols; i++ {
+	for i := offset; i < PAGE_SIZE && colIdx < numCols; i++ {
 		if page.Data[i] == 0 {
 			cols[colIdx] = string(accumulator)
 			accumulator = accumulator[:0]
 			colIdx++
+			continue
 		}
 		accumulator = append(accumulator, page.Data[i])
 	}
@@ -120,10 +115,9 @@ func (p *Pager) WriteColumns(cols []string) error {
 
 	binary.LittleEndian.PutUint32(page.Data[:4], uint32(0x474f4442)) // "GODB"
 	binary.LittleEndian.PutUint16(page.Data[4:6], uint16(1))         // version
-	binary.LittleEndian.PutUint16(page.Data[6:8], uint16(len(cols)))
-	binary.LittleEndian.PutUint32(page.Data[8:12], uint32(p.numPages))
+	binary.LittleEndian.PutUint32(page.Data[6:10], uint32(len(cols)))
 
-	offset := 12
+	offset := 10
 
 	for _, col := range cols {
 		copy(page.Data[offset:], []byte(col))
